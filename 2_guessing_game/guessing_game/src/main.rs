1f358ae9c914eba896b73def538c4797417d4dbe
theJollySin <john_science@protonmail.com>
2020-06-14 11:13:01 -0400

    finshed and cleaned up guessing game example

--- 2_guessing_game/guessing_game/src/main.rs
+++ 2_guessing_game/guessing_game/src/main.rs
@@ -1,14 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Guess a number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    println!("Guess a number! (1-100)");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+    loop {
+        // get input from user (their guesses)
+        let mut raw_guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut raw_guess)
+            .expect("Failed to read line.");
+
+        // try to parse the user-entered string as an integer. Handle bad entries.
+        let guess: u64 = match raw_guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("It doesn't look like you entered a valid number. Try again.");
+                continue;
+            }
+        };
+
+        // inform the user if they guessed too low or to high
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+    }
 }
 
